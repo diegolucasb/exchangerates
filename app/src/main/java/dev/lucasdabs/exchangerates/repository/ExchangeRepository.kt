0a0fd88dffceb42e3f9e71252a0d01b498165946
Lucas Amaral <diegolucasb@gmail.com>
2019-06-23 11:58:47 -0300

    Fixes scheduler (rx) and upgrades kotlin version

--- app/src/main/java/dev/lucasdabs/exchangerates/repository/ExchangeRepository.kt
+++ app/src/main/java/dev/lucasdabs/exchangerates/repository/ExchangeRepository.kt
@@ -1,13 +1,16 @@
 package dev.lucasdabs.exchangerates.repository
 
 import dev.lucasdabs.exchangerates.api.data.Currency
+import dev.lucasdabs.exchangerates.api.data.Exchange
 import dev.lucasdabs.exchangerates.api.service.RatesService
+import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 class ExchangeRepository(private val ratesService: RatesService) {
 
-    fun fetchData(currency: Currency) =
+    fun fetchData(currency: Currency): Observable<Exchange> =
         ratesService
             .fetchData(currency)
-            .subscribeOn(Schedulers.newThread())
-}
+            .subscribeOn(Schedulers.io())
+} 
